ddfe3c1ee63624da525d857f60e1a45a9be6f274
François HAMEAU <f.hameau@gmail.com>
2018-12-20 16:00:29 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'subsidies#index'
+
+  resources :subsidies, only: [ :index ] do
+    member do
+      resources :requests, only: [ :new, :create ]
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all subsidies"
+Subsidy.destroy_all
+
+puts "Destroying all requests"
+Request.destroy_all
+
+subsidies = [{
+    name: 'Subvention des activités sportives',
+    start_date: '1/10/2018',
+    end_date: '31/12/2018',
+    amount: 90
+  },
+  {
+    name: 'Subvention des activités culturelles',
+    start_date: '1/09/2018',
+    end_date: '31/12/2018',
+    amount: 120
+  },
+  {
+    name: 'Subvention rentrée scolaire',
+    start_date: '15/08/2018',
+    end_date: '30/09/2018',
+    amount: 60
+  },
+  {
+    name: 'Subvention des affaires de toilettes pour chat et chien',
+    start_date: '15/10/2018',
+    end_date: '15/02/2019',
+    amount: 17
+  },
+  {
+    name: 'Subvention père noël',
+    start_date: '10/12/2018',
+    end_date: '23/12/2019',
+    amount: 180
+  }
+ ]
+
+puts "Seeding subsidies"
+ subsidies.each { |subsidy| Subsidy.create!(subsidy)}
